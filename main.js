4f02503f66dc8abad7506ffbf4a1a5b1c7b8768d
Rex <wlilong@163.com>
2018-08-07 11:31:14 +0800

    增加思维导图，ppt, 和知识总结

--- main.js
+++ main.js
@@ -6,6 +6,21 @@ window.onload = function () {
   let projectItemsArr = [
     {title: '安全管理平台', desc: '传统Java Web开发', link: ''}
   ]
+  let xmindsItemsArr = [
+    { title: 'JS对象', desc: 'JS的对象关系图', link: 'xminds/Object.svg' },
+    { title: 'JS原型链', desc: 'JS的原型链关系图', link: 'xminds/Prototype.svg' },
+    { title: 'ServiceWorker', desc: 'SW内置对象概览', link: 'xminds/ServiceWorker.svg' }, 
+    { title: 'VueJS', desc: 'Vue源码流程简析', link: 'xminds/VueFlow.svg' },
+  ]
+  let pptsItemsArr = [
+    { title: 'JS对象', desc: 'JS的对象基础知识', link: 'ppts/Object.pptx' },
+    { title: 'ServiceWorker', desc: 'SW基础知识', link: 'ppts/service-worker.svg' },
+    { title: 'WebAssembly', desc: 'WebAssembly基础知识', link: 'ppts/WebAssembly-base.pptx' },
+    { title: '微信小程序', desc: '微信小程序基础', link: 'ppts/sm-program.pdf'}
+  ]
+  let knowledgesItemsArr = [
+    { title: '高效页面注意事项', desc: '高效页面注意事项', link: 'knowledges/efficient-page.html' }
+  ]
   function get$itemArr (itemsArr) {
     return itemsArr.map(function (item) {
       let $item = document.createElement('div')
@@ -31,12 +46,27 @@ window.onload = function () {
   }
   let $translateItemsArr = get$itemArr(translateItemsArr)
   let $projectItemsArr = get$itemArr(projectItemsArr)
+  let $xmindsItemsArr = get$itemArr(xmindsItemsArr)
+  let $pptsItemsArr = get$itemArr(pptsItemsArr)
+  let $knowledgesItemsArr = get$itemArr(knowledgesItemsArr)
   let $translations = document.querySelector('.translations')
-  let $projects = document.querySelector('.projects')
+  // let $projects = document.querySelector('.projects')
+  let $xminds = document.querySelector('.xminds')
+  let $ppts = document.querySelector('.ppts')
+  let $knowledges = document.querySelector('.knowledges')
   $translateItemsArr.forEach(function ($item) {
     $translations.appendChild($item)
   })
-  $projectItemsArr.forEach(function ($item) {
+  /* $projectItemsArr.forEach(function ($item) {
     $projects.appendChild($item)
+  }) */
+  $xmindsItemsArr.forEach(function ($item) {
+    $xminds.appendChild($item)
+  })
+  $pptsItemsArr.forEach(function ($item) {
+    $ppts.appendChild($item)
+  })
+  $knowledgesItemsArr.forEach(function ($item) {
+    $knowledges.appendChild($item)
   })
 }
